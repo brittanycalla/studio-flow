a5caa5b7770b67281aaf779fffad337c59b745e4
Brittany Callahan <brittanycallahanx@gmail.com>
2023-10-28 14:29:32 -0400

    add frontend assets

--- routes/main.js
+++ routes/main.js
@@ -4,7 +4,7 @@ const authController = require('../controllers/auth')
 const homeController = require('../controllers/home')
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
-// router.get('/', ensureAuth, homeController.getIndex)
+router.get('/', ensureAuth, homeController.getIndex)
 router.get('/', ensureGuest, homeController.getIndex)
 router.get('/api/user', authController.getUser)
 router.get('/api/home', ensureAuth, homeController.getHome)
