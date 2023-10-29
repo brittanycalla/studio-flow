ed74f954c89f1f94bad396c674fe679481b2664a
Brittany Callahan <brittanycallahanx@gmail.com>
2023-10-29 15:19:14 -0400

    troubleshoot app route

--- routes/main.js
+++ routes/main.js
@@ -6,7 +6,6 @@ const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 // router.get('/', ensureAuth, homeController.getIndex)
 router.get('/', ensureGuest, homeController.getIndex)
-router.get('/app*', ensureAuth, homeController.getHome)
 router.get('/api/user', authController.getUser)
 router.get('/api/home', ensureAuth, homeController.getHome)
 router.get('/login', authController.getLogin)
