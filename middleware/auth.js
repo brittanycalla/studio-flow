346a0d7a3da3a5475d55f88959456882d264e59d
Brittany Callahan <brittanycallahanx@gmail.com>
2023-10-29 14:11:46 -0400

    fix guest routing

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     ensureGuest: function (req, res, next) {
       if (req.isAuthenticated()) {
-        return res.redirect('/home')
+        return res.redirect('/app')
       }
       next()
     }
